06889ac4e5deab3520ef4b9bd7167bdb503fa5a7
Luke Wassink <lwassink@gmail.com>
2026-09-21 23:49:08 -0500

    Initialize Rust learning repo and finish Rustlings

---
+++ rustlings/exercises/15_traits/traits2.rs
@@ -0,0 +1,27 @@
+trait AppendBar {
+    fn append_bar(self) -> Self;
+}
+
+// TODO: Implement the trait `AppendBar` for a vector of strings.
+// `append_bar` should push the string "Bar" into the vector.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Vec<String> {
+        self.into_iter().chain(vec!["Bar".to_string()]).collect()
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_vec_pop_eq_bar() {
+        let mut foo = vec![String::from("Foo")].append_bar();
+        assert_eq!(foo.pop().unwrap(), "Bar");
+        assert_eq!(foo.pop().unwrap(), "Foo");
+    }
+}
